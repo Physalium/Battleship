204eb5139308a033cb40a72b741c7c279de27dae
Karol Nawrot <47122661+Physalium@users.noreply.github.com>
2022-04-24 21:40:16 +0200

    added next move button and shot count

--- BattleShipClient/battleship-client/src/containers/GameContainer/GameContainer.tsx
+++ BattleShipClient/battleship-client/src/containers/GameContainer/GameContainer.tsx
@@ -1,13 +1,16 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import React, { useState } from "react";
-import { Player } from "../../api/types/games.types";
+import { Player, Shot } from "../../api/types/games.types";
 import { generateGameApi } from "../../api/utils.game";
 import PlayerPanel from "../PlayerPanel/PlayerPanel";
+import "./styles.scss";
 
 function GameContainer() {
   const [firstPlayer, setFirstPlayer] = useState<Player>();
   const [secondPlayer, setSecondPlayer] = useState<Player>();
+  const [shotsArray, setShotsArray] = useState<Shot[]>();
+  const [shotCount, setShotCount] = useState<number>(0);
 
   const onGenerate = () => {
     generateGameApi()
@@ -15,8 +18,15 @@ function GameContainer() {
         console.log(response.data);
         setFirstPlayer(response.data.player1);
         setSecondPlayer(response.data.player2);
+        setShotsArray(response.data.shots);
       })
       .catch((data) => console.error(data));
+  };
+
+  const onNextMove = () => {
+    // let nextShot = shotsArray[shotCount];
+    // nextShot.hitPosition
+    setShotCount(shotCount + 1);
   };
   return (
     <Container maxWidth="md">
@@ -24,10 +34,26 @@ function GameContainer() {
         <Typography variant="h4" component="h1" gutterBottom>
           Battleship game
         </Typography>
-        <Button variant="contained" onClick={onGenerate}>
-          Generate new game
-        </Button>
-        {firstPlayer && <PlayerPanel player={firstPlayer} />}
+        <div className="button-panel">
+          <Button variant="contained" onClick={onGenerate}>
+            Generate new game
+          </Button>
+
+          {shotsArray && (
+            <>
+              <Typography variant="h6" component="h1" gutterBottom>
+                {`Current shot: ${shotCount}/${shotsArray.length}`}
+              </Typography>
+              <Button variant="outlined" onClick={onNextMove}>
+                {"Next shot >"}
+              </Button>
+            </>
+          )}
+        </div>
+        <div className="player-board-container">
+          {firstPlayer && <PlayerPanel player={firstPlayer} />}
+          {secondPlayer && <PlayerPanel player={secondPlayer} />}
+        </div>
       </Box>
     </Container>
   );
